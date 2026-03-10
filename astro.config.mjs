40ec3ab8feb3c4632989d307b5a264018bb84d50
Floris van Vredendaal <floris@hatless.com>
2026-03-10 11:09:37 +0100

    Fix: remove sitemap plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { siteConfig } from './src/config';
 
 export default defineConfig({
   site: siteConfig.url,
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), tailwind()],
   markdown: {
     shikiConfig: {
       theme: 'github-light',
